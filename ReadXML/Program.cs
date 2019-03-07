3ce21f96b9c3aacd4c92a32ca1fc5924521f1aa2
Josue Chinchilla Hidalgo <josuech28@gmail.com>
2019-03-07 13:54:03 -0600

    Initial Read XML

---
+++ ReadXML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ReadXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            XmlDocument file;
+            char c = '\u005c';
+            // Cambiar ruta a algun xaml que tengan local ustedes
+            String ruta = "C:" + c + "Users" + c + "GQ925VN" + c + "Documents" + c + "UiPath" + c + "ChinoTime" + c + "QA.xaml";
+            Console.WriteLine(ruta);
+            ReadXML readXML = new ReadXML();
+            readXML.Path = ruta;
+            readXML.loadXML();
+            readXML.readChildNodes();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReadXML/ReadXML.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ReadXML
+{
+    class ReadXML
+    {
+        private XmlDocument document;
+        private string path;
+
+        public ReadXML()
+        {
+            document  = new XmlDocument();
+        }
+
+        public string Path {
+            get { return path; }
+            set { path = value; }
+        }
+
+        public XmlDocument Document
+        {
+            get { return document; }
+            set { document = value; }
+        }
+
+        public XmlDocument loadXML()
+        {
+            try
+            {
+                document.Load(path);
+                return document;
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"C:\text.txt", path + "\r\n\r\n" + ex.Message);
+                throw ex;
+            }
+        }
+
+        public XmlNode findNode(string nodePath)
+        {
+            XmlNode node = document.DocumentElement.SelectSingleNode(nodePath);
+            return node;
+        }
+
+        public string getNodeText(XmlNode node)
+        {
+            return node.InnerText;
+        }
+
+        public string readAttribute(XmlNode node, string attributeName)
+        {
+            string attribute = node.Attributes[attributeName]?.InnerText;
+            return attribute;
+        }
+
+        public void readChildNodes()
+        {
+            foreach (XmlNode node in document.DocumentElement.ChildNodes)
+            {
+   
+                string nodeContent = node.InnerText;
+                string nodeXML = node.Name;
+                Console.WriteLine("XML -> " + nodeXML + " Content: " + nodeContent);
+                if (node.HasChildNodes)
+                {
+                    foreach(XmlNode children in node)
+                    {
+                        nodeContent = children.InnerText;
+                        nodeXML = children.Name;
+                        Console.WriteLine("XML Children -> " + nodeXML + " Content children: " + nodeContent);
+                    }
+                }
+            }
+        }
+
+    }
+}
